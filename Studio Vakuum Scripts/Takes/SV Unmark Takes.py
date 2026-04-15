e0f919d5c056e0af8509760c6ea8c29f80b91532
StudioVakuum <yannick.neuhaus@studio-vakuum.com>
2026-04-15 09:27:31 +0200

    1.1.0
    - Added undo function
    - restrict to takes and its children, when selected

--- Studio Vakuum Scripts/Takes/SV Unmark Takes.py
+++ Studio Vakuum Scripts/Takes/SV Unmark Takes.py
@@ -1,58 +1,47 @@
 """
 SV Unmark Takes
-
 Author: Yannick Neuhaus (Studio Vakuum)
 Website: https://www.studio-vakuum.com
-Version: 1.0.0
-Description-US:Unmarks all takes in the take system.
-
-Written for Maxon Cinema 4D 2024.5.1
+Version: 1.1.0
+Description-US:Unmarks all takes in the take system or only the selected ones (and their children).
+Written for Maxon Cinema 4D 2026.1.4
 Python version 3.11.4
 """
-
 import c4d
 
-def mark_last_level_takes(takeData):
+def unmark_takes(takeData, selected_takes=None):
     mainTake = takeData.GetMainTake()
     if not mainTake:
         return
-
-    def mark_last_level_recursive(take):
-        if not take.GetDown():
-            take.SetChecked(True)
-        else:
-            child = take.GetDown()
-            while child:
-                mark_last_level_recursive(child)
-                child = child.GetNext()
-
-    mark_last_level_recursive(mainTake)
+    
+    def unmark_recursive(take):
+        if selected_takes is None or take in selected_takes:
+            take.SetChecked(False)
+        
+        child = take.GetDown()
+        while child:
+            unmark_recursive(child)
+            child = child.GetNext()
+    
+    unmark_recursive(mainTake)
 
 def main():
     doc = c4d.documents.GetActiveDocument()
     takeData = doc.GetTakeData()
-    
     if not takeData:
         return
+
+    selected_takes = takeData.GetTakeSelection(True)
+    if not selected_takes:
+        selected_takes = None
+
+    doc.StartUndo()
+    doc.AddUndo(c4d.UNDOTYPE_CHANGE, takeData.GetMainTake())
     
-    mark_all_takes(takeData)
-
+    unmark_takes(takeData, selected_takes)
+    
+    doc.EndUndo()
     c4d.EventAdd()
-
-def mark_all_takes(takeData):
-    mainTake = takeData.GetMainTake()
-    if not mainTake:
-        return
-
-    def mark_takes_recursive(take):
-        take.SetChecked(False)
-
-        child = take.GetDown()
-        while child:
-            mark_takes_recursive(child)
-            child = child.GetNext()
-
-    mark_takes_recursive(mainTake)
 
 if __name__ == '__main__':
     main()
